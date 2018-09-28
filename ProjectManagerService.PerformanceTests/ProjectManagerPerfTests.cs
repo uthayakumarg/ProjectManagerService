5c537f42f21cfcada574dcfadd4b596cac943a33
uthayakumarg <uthayakumarg@gmail.com>
2018-09-28 16:19:31 +0530

    Performance test cases

---
+++ ProjectManagerService.PerformanceTests/ProjectManagerPerfTests.cs
@@ -0,0 +1,60 @@
+﻿using NBench;
+using System.Collections.Generic;
+using ProjectManagerEntity;
+using ProjectManagerBL;
+using ProjectManagerDL;
+using System;
+
+namespace ProjectManagerService.PerformanceTests
+{
+    public class ProjectManagerPerfTests
+    {
+        private IUserBL _userService;
+        private IProjectBL _projectService;
+        private ITaskBL _taskService;
+
+        private TaskEntity _newTask;
+
+        [PerfSetup]
+        public void Setup(BenchmarkContext context)
+        {
+            IUserDataLayer _userRepo = new UserDataLayer();
+            IProjectDataLayer _projectRepo = new ProjectDataLayer();
+            ITaskDataLayer _taskRepo = new TaskDataLayer();
+
+            _userService = new UserBL(_userRepo);
+            _projectService = new ProjectBL(_projectRepo);
+            _taskService = new TaskBL(_taskRepo);
+
+            _newTask = new TaskEntity { TaskName = "Perf Test Task", ParentId = 6, Priority = 10, StartDate = "10/01/2018", EndDate = "10/11/2018", ProjectId = 3, UserId = 2068759 };
+        }
+
+        [PerfBenchmark(NumberOfIterations = 500, RunMode = RunMode.Iterations, TestMode = TestMode.Test, SkipWarmups = true)]
+        [ElapsedTimeAssertion(MaxTimeMilliseconds = 100)]
+        public void Get_Tasks_500_Iterations()
+        {
+            _taskService.GetAllTasks(3);
+        }
+
+        [PerfBenchmark(RunMode = RunMode.Throughput, RunTimeMilliseconds = 10000, TestMode = TestMode.Test, SkipWarmups = true)]
+        [ElapsedTimeAssertion(MaxTimeMilliseconds = 3000)]
+        public void Get_Tasks_10_Minutes()
+        {
+            _taskService.GetAllTasks(3);
+        }
+
+        [PerfBenchmark(NumberOfIterations = 1, RunMode = RunMode.Iterations, TestMode = TestMode.Test, SkipWarmups = true)]
+        [ElapsedTimeAssertion(MaxTimeMilliseconds = 50)]
+        public void Add_Task_Elapsed_Time()
+        {
+            _taskService.AddTask(_newTask);
+        }
+
+        [PerfBenchmark(NumberOfIterations = 1, RunMode = RunMode.Iterations, TestMode = TestMode.Test, SkipWarmups = true)]
+        [MemoryAssertion(MemoryMetric.TotalBytesAllocated, MustBe.GreaterThan, ByteConstants.SixtyFourKb)]
+        public void Add_Task_Memory_Consumed()
+        {
+            _taskService.AddTask(_newTask);
+        }
+    }
+}
---
+++ ProjectManagerService.PerformanceTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProjectManagerService.PerformanceTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjectManagerService.PerformanceTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7352491c-94eb-4176-9a6a-0d0525ca832e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
